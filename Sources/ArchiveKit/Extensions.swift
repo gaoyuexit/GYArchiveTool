b186b64a3991be7d60520b94cd0f4a8d3cb8e5ea
gaoyu <gaoyuexit@163.com>
2017-06-12 15:36:33 +0800

    modify name GY remove

---
+++ Sources/ArchiveKit/Extensions.swift
@@ -0,0 +1,53 @@
+//
+//  Extensions.swift
+//  GYReduceTool
+//
+//  Created by 郜宇 on 2017/5/25.
+//
+//
+
+import Foundation
+import PathKit
+import Rainbow
+
+extension Path {
+    
+    var size: Int {
+        if isDirectory {
+            let childrenPaths = try? children()
+            return (childrenPaths ?? []).reduce(0){ $0 + $1.size }
+        }else{
+            // 隐藏文件
+            if lastComponent.hasPrefix(".") { return 0 }
+            let attr = try? FileManager.default.attributesOfItem(atPath: absolute().string)
+            if let num = attr?[.size] as? NSNumber {
+                return num.intValue
+            }else {
+                return 0
+            }
+        }
+    }
+}
+
+public extension String {
+    var error: String { return self.red.bold }
+    var pass: String { return self.green.bold }
+}
+
+public extension Process {
+    
+    func execute(args: [String]) {
+        arguments = args
+        launch()
+        print(("Excusing ->" + "\(args)" + "<- Commond\n").pass)
+        waitUntilExit()
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Tests/ArchiveToolTests/ArchiveKitTests.swift
@@ -0,0 +1,39 @@
+//
+//  GYArchiveToolTests.swift
+//  GYArchiveToolTests
+//
+//  Created by 郜宇 on 2017/6/7.
+//
+//
+
+import XCTest
+
+@testable import ArchiveKit
+
+class ArchiveKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    func testArchiveKitSpecs() {
+        specArchiveKit()
+    }
+}
